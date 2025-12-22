13902842b3af40c93138f1e84c576937618dc62b
User <User@DESKTOP-QHG18FL>
2025-12-22 12:17:59 +0300

    DHCPD DONE

--- Inheritance/WOL/main.cpp
+++ Inheritance/WOL/main.cpp
@@ -41,6 +41,41 @@ void main()
 	system("notepad 201 WOL.txt");
 #endif // WOL
 
+	std::ifstream fin("201 RAW.txt");
+	std::ofstream fout("201.dhcpd");
+	if (fin.is_open())
+	{
+		const int IP_BUFFER_SIZE = 16;
+		const int MAC_BUFFER_SIZE = 18;
+		char sz_IP_buffer[IP_BUFFER_SIZE] = {};
+		char sz_MAC_buffer[MAC_BUFFER_SIZE] = {};
+		for (int i = 1; !fin.eof(); i++)
+		{
+			fin >> sz_IP_buffer >> sz_MAC_buffer;
+			if (sz_IP_buffer[0] == 0 || sz_MAC_buffer[0] == 0)continue;
+			for (int i = 0; sz_MAC_buffer[i]; i++)
+				if (sz_MAC_buffer[i] == '-')sz_MAC_buffer[i] = ':';
+			cout << "host-" << i << endl;
+			cout << "{\n";
+			cout << "\thardware ethernet\t" << sz_MAC_buffer << ";\n";
+			cout << "\tfixed-address\t\t" << sz_IP_buffer << ";\n";
+			cout << "}\n";
+			cout << endl;
 
+			fout << "host-" << i << endl;
+			fout << "{\n";
+			fout << "\thardware ethernet\t" << sz_MAC_buffer << ";\n";
+			fout << "\tfixed-address\t\t" << sz_IP_buffer << ";\n";
+			fout << "}\n";
+			fout << endl;
 
+		}
+	}
+	else
+	{
+		std::cerr << "Error: file not found" << endl;
+	}
+	fout.close();
+	fin.close();
+	system("notepad 201.dhcpd");
 }
