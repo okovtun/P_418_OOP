8735c4beb500879321bb1d976dff9eb9a1ada8c5
User <User@DESKTOP-QHG18FL>
2026-02-04 12:24:20 +0300

    ForwardList insert DONE

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -50,6 +50,65 @@ public:
 		//3) Смещаем Голову на Новый элемент:
 		Head = New;
 	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr)return push_front(Data);
+
+		//1) Создаем новый элемент:
+		Element* New = new Element(Data);
+
+		//2) Доходим до конца списка:
+		Element* Temp = Head;
+		while (Temp->pNext)Temp = Temp->pNext;
+
+		//3) Добавляем элемент в конец списка:
+		Temp->pNext = New;
+	}
+
+	void insert(int Data, int Index)
+	{
+		if (Index == 0) return push_front(Data);
+
+		//1) Создаем новый элемент:
+		Element* New = new Element(Data);
+
+		//2) Доходим до нужного элемента:
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)
+		{
+			if (Temp->pNext == nullptr)break;
+			Temp = Temp->pNext;
+		}
+
+		//3) Добавляем элемент в список:
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
+	}
+
+	//				Removing elements:
+	void pop_front()
+	{
+		//1) Запоминаем адрес удаляемого элемента:
+		Element* Erased = Head;
+
+		//2) Смещаем Голову на следующий элемент:
+		Head = Head->pNext;
+
+		//3) Удаляем удаляемый элемент из памяти:
+		delete Erased;
+	}
+	void pop_back()
+	{
+		//1) Доходим до предпоследнего элемента списка:
+		Element* Temp = Head;
+		while (Temp->pNext->pNext)Temp = Temp->pNext;
+
+		//2) Удаляем удаляемый элемент:
+		delete Temp->pNext;
+
+		//3) Зануляем указатель на последний элемент в предпоследнем элементе:
+		Temp->pNext = nullptr;
+	}
 
 	//				Methods:
 	void print()const
@@ -74,7 +133,17 @@ void main()
 	ForwardList list;
 	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		//list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
 	}
+	list.print();
+	//list.push_back(123);
+	//list.pop_front();
+	//list.pop_back();
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(value, index);
 	list.print();
 }
