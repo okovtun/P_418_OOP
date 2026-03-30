d9597acb02c9912e22e85b59fe2ee15828b79997
User <User@DESKTOP-QHG18FL>
2026-03-30 11:50:58 +0300

    AVG DONE

--- DataContainers/BinaryTree/main.cpp
+++ DataContainers/BinaryTree/main.cpp
@@ -24,12 +24,92 @@ class Tree
 		{
 			cout << "EDestructor:\t" << this << endl;
 		}
+		friend class Tree;
 	}*Root;
 public:
-	void insert()
+	Element* getRoot()
+	{
+		return Root;
+	}
+	Tree() :Root(nullptr)
+	{
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Tree()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+
+	int minValue(Element* Root)const
+	{
+		if (this->Root == nullptr)return 0;
+		return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+		/*if (Root->pLeft == nullptr)return Root->Data;
+		else return minValue(Root->pLeft);*/
+	}
+	int maxValue(Element* Root)const
+	{
+		if (this->Root == nullptr)return 0;
+		return Root->pRight == nullptr ? Root->Data : maxValue(Root->pRight);
+		/*if (Root->pRight == nullptr) return Root->Data;
+		else return maxValue(Root->pRight);*/
+	}
+	int sum(Element* Root)const
+	{
+		return Root == nullptr ? 0 : sum(Root->pLeft) + sum(Root->pRight) + Root->Data;
+		/*if (Root == nullptr)return 0;
+		else return sum(Root->pLeft) + sum(Root->pRight) + Root->Data;*/
+	}
+	int count(Element* Root)const
+	{
+		return Root == nullptr ? 0 : count(Root->pLeft) + count(Root->pRight) + 1;
+	}
+	double avg()const
+	{
+		return (double)sum(Root) / count(Root);
+	}
+
+	void print(Element* Root)const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << "\t";
+		print(Root->pRight);
+	}
 };
 
 void main()
 {
-
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер дерева: "; cin >> n;
+	Tree tree;
+	cout << "Минимальное значение в дереве: " << tree.minValue(tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.getRoot()) << endl;
+	for (int i = 0; i < n; i++)
+	{
+		tree.insert(rand() % 100 - 50, tree.getRoot());
+	}
+	tree.print(tree.getRoot());
+	cout << endl;
+	cout << "Минимальное значение в дереве: " << tree.minValue(tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.getRoot()) << endl;
+	cout << "Сумма элементов дерева: " << tree.sum(tree.getRoot()) << endl;
+	cout << "Количество элементов дерева: " << tree.count(tree.getRoot()) << endl;
+	cout << "Среднее-арифметическое элементов дерева: " << tree.avg() << endl;
 }
