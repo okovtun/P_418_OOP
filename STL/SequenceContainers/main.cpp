fba784c56d266d454c57461f479c171037b17669
User <User@DESKTOP-QHG18FL>
2026-04-15 12:58:17 +0300

    Vector in progress

---
+++ STL/SequenceContainers/main.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<array>
+#include<vector>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n-----------------------------------\n"
+
+template<typename T>void vector_properties(const std::vector<T>& vec)
+{
+	cout << "Size:    " << vec.size() << endl;		//фактический размер
+	cout << "Capacity:" << vec.capacity() << endl;	//вместительность
+	cout << "MaxSize: " << vec.max_size() << endl;	//максимально-возможный размер
+}
+
+//#define STL_ARRAY
+#define STL_VECTOR
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef STL_ARRAY
+	//array - это контейнер, который хранит данные в виде статического массива.
+	std::array<int, 5> arr = { 3, 5, 8, 13, 21 };
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // STL_ARRAY
+
+#ifdef STL_VECTOR
+	//vector - это контейнер, который хранит данные в виде динамического массива.
+	std::vector<int> i_vec(10); //= { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	vector_properties(i_vec);
+	i_vec.push_back(55);
+	i_vec.push_back(89);
+	vector_properties(i_vec);
+
+	//std::vector<double> d_vec;
+	//vector_properties(d_vec);
+	//	IA-32
+#endif // STL_VECTOR
+
+
+}
